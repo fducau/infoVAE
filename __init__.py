98f70a36b2ea3923a75dcfb4a7b66f4d8bd5de16
fducau <fnd212@nyu.edu>
2016-11-25 13:21:02 -0500

    First commit

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: fnd
-# @Date:   2016-11-25 12:30:19
-# @Last Modified by:   fnd
-# @Last Modified time: 2016-11-25 12:30:19
---
+++ viz.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+from vae import *
+
+
+
+
+
+
